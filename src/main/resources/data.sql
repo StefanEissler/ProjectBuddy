0ce91e837a2c5304124e8cc9c1d1a6e8fdf4e673
StefanEissler <stefaneissler@web.de>
2022-11-08 21:24:15 +0100

    Sample Data in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO budgets(id, name, date, amount) VALUES(1, 'Project1', '2022-10-02', 1000.00);
+INSERT INTO bookings(id, title, timestamp, amount, category, budget_id) VALUES ( 2, 'Fahrtkosten', '2022-11-08', 250.00, 'Cost', 1);
+INSERT INTO bookings(id, title, timestamp, amount, category, budget_id) VALUES ( 3, 'Belege', '2022-12-24', 100.00, 'Forecast', 1);
